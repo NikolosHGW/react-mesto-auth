47236b48ce135865d39acda7b1b0295ffe90f6ca
Ник Николос <nic23173@gmail.com>
2021-04-06 12:48:15 +0300

    fix: delete reseting inputs in AuthForm

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -18,7 +18,6 @@ export default function AuthForm({ title, name, textButton, onSubmit, children }
     evt.preventDefault();
     const { email, password } = userData;
     onSubmit(email, password);
-    setUserData({email: '', password: ''});
   }
 
   return (
@@ -34,8 +33,6 @@ export default function AuthForm({ title, name, textButton, onSubmit, children }
           <label className="auth__field">
             <input
               id="email-input"
-              // className={`auth__input auth__input_el_email${
-              //   info.nameValidMessage ? ' popup__input_type_error' : ''}`}
               className="auth__input"
               placeholder="Email"
               value={userData.email}
@@ -46,17 +43,10 @@ export default function AuthForm({ title, name, textButton, onSubmit, children }
               minLength="2"
               maxLength="40"
             />
-            {/* <span className={`popup__input-error name-input-error${
-              info.nameValid ? '' : ' popup__input-error_active'}`}
-            >
-              {info.nameValid ? '' : info.nameValidMessage}
-            </span> */}
           </label>
           <label className="auth__field">
             <input
               id="password-input"
-              // className={`auth__input auth__input_el_job${
-              //   info.descriptionValidMessage ? ' auth__input_type_error' : ''}`}
               className="auth__input"
               placeholder="Пароль"
               value={userData.password}
@@ -67,23 +57,12 @@ export default function AuthForm({ title, name, textButton, onSubmit, children }
               minLength="2"
               maxLength="200"
             />
-            {/* <span className={`popup__input-error job-input-error${
-              info.descriptionValid ? '' : ' popup__input-error_active'}`}
-            >
-              {info.descriptionValid ? '' : info.descriptionValidMessage}
-            </span> */}
           </label>
         </fieldset>
         <button
-          // className={`auth__save-button${
-          //   isValid ? '' : ' auth__save-button_inactive'}`}
           className="auth__save-button"
-          // disabled={!isValid}
           type="submit"
         >
-          {/* {isLoading ? (
-            <div className="popup__spinner"></div>
-            ) : textButton} */}
           {textButton}
         </button>
       </form>
