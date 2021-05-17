f43ded06215100290b00c56082f4f2cbb89beab0
Ник Николос <nic23173@gmail.com>
2021-05-17 13:25:17 +0300

    fix: delete HashRouter and change homepage and change deploy script

--- src/index.js
+++ src/index.js
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App.js';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <HashRouter basename="/">
-        <App />
-      </HashRouter>
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
